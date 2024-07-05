73253b87076e4cf703ca44f1fd3aa520ab338e76
Adnaan nanda <adnaan.nanda@outlook.com>
2024-07-05 16:33:35 +0530

    Add files via upload

---
+++ DAY 1/Q4.input.py
@@ -0,0 +1,10 @@
+#taking inpput from the user
+number=int(input("Enter a number:"))
+#chceking condtions using if else statment
+if number>0 :
+    print("the number is positive")
+elif number<0:
+    print("the number is negative") 
+else:
+    print ("number is zero")
+    
